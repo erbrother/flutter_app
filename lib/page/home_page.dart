c83be65c456387e3b15402e023bf4eeb3428320b
erbrother <15625129961@163.com>
2019-11-15 17:31:49 +0800

    update

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _userAgreement = false;
   final TextEditingController _controller = new TextEditingController();
 
   @override
@@ -14,7 +15,7 @@ class _HomePageState extends State<HomePage> {
     return new Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 35.0),
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage("lib/images/backimg.jpg"),
@@ -26,11 +27,12 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new Container(
-                  margin: EdgeInsets.only(top: 50.0),
-                  padding: EdgeInsets.all(15.0),
+                  margin: EdgeInsets.only(top: 25.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 15.0, vertical: 25.0),
                   decoration: new BoxDecoration(
                     color: Color.fromRGBO(0, 0, 0, 0.7),
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
                   ),
                   child: new Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,7 +41,7 @@ class _HomePageState extends State<HomePage> {
                       _textItem("尊敬的用户您好，感谢您采购了启辰智能耳机，为了给您优质的内容服务，需要您激活耳机。"),
                       _textItem("请找到耳机，查看耳机上方的条形码进行耳机激活"),
                       new InkWell(
-                        onTap: () => print("click"),
+                        onTap: () => changeUserAgreement(),
                         child: new Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
@@ -48,11 +50,11 @@ class _HomePageState extends State<HomePage> {
                               height: 20.0,
                               margin: EdgeInsets.only(right: 10.0),
                               color: Colors.white,
-                              child: new Icon(
+                              child: _userAgreement ? new Icon(
                                 Icons.check,
                                 color: Colors.green,
                                 size: 20.0,
-                              ),
+                              ) : new Container(),
                             ),
                             _textItem("请仔细阅读此信息"),
                             new InkWell(
@@ -71,18 +73,18 @@ class _HomePageState extends State<HomePage> {
                             new Expanded(
                               child: new Container(
                                   padding:
-                                      EdgeInsets.symmetric(horizontal: 12.0),
+                                      EdgeInsets.symmetric(horizontal: 30.0),
                                   height: 36.0,
                                   decoration: BoxDecoration(
                                       color: Colors.white,
                                       borderRadius: BorderRadius.all(
-                                          Radius.circular(15.0))),
+                                          Radius.circular(25.0))),
                                   child: new TextField(
                                     controller: _controller,
                                     decoration: new InputDecoration(
                                         hintText: "输入设备ID登录",
                                         contentPadding:
-                                            EdgeInsets.only(top: 6.0),
+                                            EdgeInsets.only(top: 5.0),
                                         border: InputBorder.none),
                                   )),
                             ),
@@ -106,10 +108,9 @@ class _HomePageState extends State<HomePage> {
                 new Column(
                   children: <Widget>[
                     new InkWell(
-                      hoverColor: Colors.green,
-                      focusColor: Colors.red,
-                      onTap: () => print("ok"),
+                      onTap: () => connectDevice(),
                       child: new Container(
+                        margin: EdgeInsets.only(bottom: 15.0),
                         padding: EdgeInsets.symmetric(
                             horizontal: 50.0, vertical: 12.0),
                         decoration: BoxDecoration(
@@ -122,19 +123,10 @@ class _HomePageState extends State<HomePage> {
                         child: new Text("连接设备"),
                       ),
                     ),
-                    new RaisedButton(
-                      onPressed: () => print('rasie button'),
-                      child: new Container(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 50.0, vertical: 12.0),
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [Color(0xfffffb67), Color(0xfffeab4b)]),
-                            borderRadius:
-                            BorderRadius.all(Radius.circular(20.0))),
-                        child: new Text("连接设备"),
+                    new InkWell(
+                      child: new Text(
+                        "没有设备",
+                        style: TextStyle(color: Colors.white),
                       ),
                     )
                   ],
@@ -144,11 +136,21 @@ class _HomePageState extends State<HomePage> {
           )),
     );
   }
-}
 
-Widget _textItem(str) {
-  return new Container(
-      margin: EdgeInsets.only(bottom: 12.0),
-      child: new Text("$str", style: TextStyle(color: Colors.white)));
-  ;
+  Widget _textItem(str) {
+    return new Container(
+        margin: EdgeInsets.only(bottom: 12.0),
+        child: new Text("$str", style: TextStyle(color: Colors.white)));
+  }
+
+  void changeUserAgreement() {
+    setState(() {
+      _userAgreement = !_userAgreement;
+    });
+  }
+  //连接设备
+  void connectDevice() {
+    print("连接设备：${_controller.text}");
+    print("用户协议：${_userAgreement}");
+  }
 }
